335f9b96bfb302fb532f88dd7b1bec5d991093c4
jtb110 <jtb110@hotmail.com>
2015-11-08 14:08:46 +0100

    changes made to all views

--- AngSeedClient/app/view5/view5.js
+++ AngSeedClient/app/view5/view5.js
@@ -1,16 +1,38 @@
 'use strict';
-
 angular.module('myApp.view5', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view5', {
-    templateUrl: 'view5/view5.html',
-    controller: 'View5Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5', {
+                    templateUrl: 'view5NewUser/view5.html',
+                    controller: 'View5Ctrl'
+                });
+            }])
 
-.controller('View5Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+        .controller('View5Ctrl', function ($http) {
+            var self = this;
+            self.userName = "";
+            self.password = "";
+            self.createUser = function () {
+                $http({
+                    method: 'POST',
+                    url: 'api/allUsers',
+                    headers: {
+                        'Content-Type': "application/json"
+                    },
+                    data: {userName: self.userName,
+                        password: self.password
+                    }
+
+
+                }).then(function successCallback(response) {
+                    self.userName = "";
+                    self.password = "";
+                    alert('new user created');
+
+                }, function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                })
+            };
+
+        });
